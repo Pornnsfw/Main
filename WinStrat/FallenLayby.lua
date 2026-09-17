dfd337454566ec4b8b558ce849b306e4d8df04fc
AX <pornnsfwsafe@gmail.com>
2026-09-17 08:30:26 -0700

    Create FallenLayby.lua

---
+++ WinStrat/FallenLayby.lua
@@ -0,0 +1,173 @@
+
+TDS:Place("Trapper", 2.787752151489258, 243, 207.21592712402344, true)
+TDS:Ready()
+
+-- [ Wave 1 ] --
+TDS:Upgrade(1)
+
+-- [ Wave 2 ] --
+TDS:Place("Trapper", -2.9335803985595703, 242.99998474121094, 207.55882263183594, true)
+
+-- [ Wave 3 ] --
+TDS:Upgrade(2)
+
+-- [ Wave 8 ] --
+TDS:Place("Gatling Gun", 3.063350200653076, 243, 154.9819793701172, true)
+
+-- [ Wave 9 ] --
+TDS:Upgrade(3)
+
+-- [ Wave 12 ] --
+TDS:Upgrade(3)
+
+-- [ Wave 15 ] --
+TDS:Upgrade(3)
+
+-- [ Wave 18 ] --
+TDS:Place("Medic", 2.8064403533935547, 243, 158.52227783203125, true)
+TDS:Place("Medic", 2.80936336517334, 243, 160.26649475097656, true)
+TDS:Place("Medic", 2.6769356727600098, 243, 161.93191528320312, true)
+TDS:Place("Medic", 2.749760150909424, 243, 163.50462341308594, true)
+TDS:Place("Mercenary Base", -3.0883684158325195, 243, 156.11935424804688, true)
+TDS:Place("Mercenary Base", -3.1120758056640625, 243, 159.96717834472656, true)
+
+-- [ Wave 19 ] --
+TDS:Place("Mercenary Base", -3.1629624366760254, 243, 163.60678100585938, true)
+
+-- [ Wave 20 ] --
+TDS:Upgrade(3)
+
+-- [ Wave 21 ] --
+TDS:Upgrade(4)
+TDS:Upgrade(4)
+TDS:Upgrade(4)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+
+-- [ Wave 22 ] --
+TDS:Upgrade(6)
+TDS:Upgrade(6)
+TDS:Upgrade(6)
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+
+-- [ Wave 23 ] --
+TDS:Upgrade(8)
+TDS:SetOption(8, "Unit 1", "Riot Guard")
+TDS:SetOption(8, "Unit 2", "Riot Guard")
+TDS:SetOption(8, "Unit 3", "Riot Guard")
+
+-- [ Wave 24 ] --
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+
+-- [ Wave 25 ] --
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+TDS:SetOption(9, "Unit 1", "Riot Guard")
+TDS:SetOption(9, "Unit 2", "Riot Guard")
+TDS:SetOption(9, "Unit 3", "Riot Guard")
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+
+-- [ Wave 26 ] --
+TDS:Upgrade(10)
+TDS:SetOption(10, "Unit 1", "Riot Guard")
+TDS:SetOption(10, "Unit 2", "Riot Guard")
+TDS:SetOption(10, "Unit 3", "Riot Guard")
+
+-- [ Wave 29 ] --
+TDS:Upgrade(3)
+TDS:Place("Trapper", -3.863126754760742, 242.99998474121094, 206.8181915283203, true)
+TDS:Place("Trapper", 3.8133316040039062, 243, 206.69448852539062, true)
+TDS:Place("Trapper", 3.073762893676758, 242.99998474121094, 212.127685546875, true)
+TDS:Place("Trapper", -3.178312301635742, 242.99998474121094, 213.02601623535156, true)
+
+-- [ Wave 30 ] --
+TDS:Place("Trapper", 3.150996208190918, 243.00001525878906, 216.96963500976562, true)
+
+-- [ Wave 31 ] --
+TDS:Upgrade(3)
+TDS:Upgrade(4)
+TDS:Upgrade(4)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+
+-- [ Wave 32 ] --
+TDS:Upgrade(6)
+TDS:Upgrade(6)
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+
+-- [ Wave 33 ] --
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:Upgrade(9)
+
+-- [ Wave 34 ] --
+TDS:Upgrade(9)
+TDS:Upgrade(10)
+
+-- [ Wave 35 ] --
+TDS:Upgrade(10)
+TDS:Upgrade(12)
+
+-- [ Wave 36 ] --
+TDS:Upgrade(12)
+TDS:Upgrade(12)
+TDS:Upgrade(12)
+TDS:SetOption(12, "Trap", "Bear Traps")
+TDS:Upgrade(1)
+TDS:Upgrade(1)
+TDS:Upgrade(1)
+TDS:SetOption(1, "Trap", "Bear Traps")
+TDS:Upgrade(2)
+TDS:Upgrade(2)
+TDS:Upgrade(2)
+TDS:SetOption(2, "Trap", "Bear Traps")
+TDS:Upgrade(11)
+TDS:Upgrade(11)
+TDS:Upgrade(11)
+TDS:Upgrade(11)
+TDS:SetOption(11, "Trap", "Bear Traps")
+TDS:Upgrade(14)
+TDS:Upgrade(14)
+TDS:Upgrade(14)
+TDS:Upgrade(14)
+TDS:SetOption(14, "Trap", "Landmine")
+
+-- [ Wave 37 ] --
+TDS:Upgrade(13)
+TDS:Upgrade(13)
+TDS:Upgrade(13)
+TDS:Upgrade(13)
+TDS:SetOption(13, "Trap", "Landmine")
+TDS:Upgrade(15)
+TDS:Upgrade(15)
+TDS:Upgrade(15)
+TDS:Upgrade(15)
+TDS:SetOption(15, "Trap", "Landmine")
+TDS:VoteSkip(37)
+
+-- [ Wave 38 ] --
+TDS:Place("Hacker", -2.9658727645874023, 242.99998474121094, 279.7891845703125, true)
+TDS:Place("Hacker", 3.0083227157592773, 242.99998474121094, 269.8253479003906, true)
+TDS:Upgrade(17)
+TDS:Upgrade(17)
+TDS:Upgrade(17)
+TDS:Upgrade(17)
+TDS:Upgrade(17, 2)
+TDS:Upgrade(16)
+TDS:Upgrade(16)
+TDS:Upgrade(16)
+TDS:Upgrade(16)
+TDS:Upgrade(16, 2)
+
+-- [ Wave 39 ] --
+TDS:Ability(16, "Hologram Tower", {towerPosition = Vector3.new(-3.2649013996124268, 263, 156.18722534179688), towerToClone = 3}, true)

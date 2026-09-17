9f0e4aba9702ab2aaedc5789a2fa1c0b90ed2c09
AX <pornnsfwsafe@gmail.com>
2026-09-17 08:46:21 -0700

    Update MoltenLayby.lua

--- WinStrat/MoltenLayby.lua
+++ WinStrat/MoltenLayby.lua
@@ -2,6 +2,7 @@ local Globals = getgenv()
 Globals.AutoGatling = true
 Globals.AutoMercenary = true
 
+TDS:Loadout("Trapper", "Gatling Gun", "Medic", "Mercenary Base", "Hacker")
 TDS:Place("Trapper", 2.810445785522461, 243, 207.17857360839844, true)
 TDS:Ready()
 

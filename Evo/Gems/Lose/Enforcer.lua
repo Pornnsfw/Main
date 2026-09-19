c6e464031070b44978a5dec00821a1dc8031ebdc
AX <pornnsfwsafe@gmail.com>
2026-09-19 03:32:33 -0700

    Update Enforcer.lua

--- Evo/Gems/Lose/Enforcer.lua
+++ Evo/Gems/Lose/Enforcer.lua
@@ -1,1 +1,120 @@
+local Globals = getgenv()
+Globals.AutoSkip = true
 
+TDS:Loadout("Assassin", "Soldier", "Shotgunner", "EvolvedEnforcer", "")
+TDS:RemoveIndex()
+TDS:Ready()
+TDS:Place("Farm", -10.482913970947266, 21.5455634593963623, 26.13408088684082)
+TDS:Place("Farm", -10.490318298339844, 21.503326416015625, 30.04276466369629)
+TDS:Place("Farm", -10.575254440307617, 21.43111252784729, 33.81802749633789)
+TDS:Upgrade(1)
+
+-- [ Wave 1 ] --
+TDS:Upgrade(2)
+TDS:Upgrade(3)
+
+-- [ Wave 2 ] --
+TDS:Upgrade(1)
+
+-- [ Wave 3 ] --
+TDS:Upgrade(2)
+TDS:Place("Boomerang", 10.774340629577637, 21.0205261707305908, 22.184646606445312)
+
+-- [ Wave 4 ] --
+TDS:Upgrade(4)
+TDS:Upgrade(3)
+
+-- [ Wave 5 ] --
+TDS:Upgrade(4)
+
+-- [ Wave 6 ] --
+TDS:Place("Farm", -14.130766868591309, 21.4659173488616943, 33.88728713989258)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+
+-- [ Wave 7 ] --
+TDS:Upgrade(4)
+
+-- [ Wave 8 ] --
+TDS:Place("Farm", -14.181255340576172, 21.5564073324203491, 30.144771575927734)
+TDS:Upgrade(6)
+TDS:Upgrade(1)
+
+-- [ Wave 9 ] --
+TDS:Upgrade(2)
+
+-- [ Wave 10 ] --
+TDS:Upgrade(3)
+TDS:Place("Crook Boss", 6.545931816101074, 21.3879753351211548, 27.775545120239258)
+
+-- [ Wave 12 ] --
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+
+-- [ Wave 13 ] --
+TDS:Place("Farm", -14.477234840393066, 21.5812714099884033, 26.301362991333008)
+TDS:Place("Farm", -18.002553939819336, 21.473349690437317, 33.825927734375)
+TDS:Place("Farm", -18.1511287689209, 21.4703178405761719, 30.091886520385742)
+TDS:Place("Farm", -18.264617919921875, 21.4667580127716064, 26.444168090820312)
+
+-- [ Wave 14 ] --
+TDS:Place("Farm", -11.14169692993164, 21.3210381269454956, 37.81104278564453)
+TDS:Upgrade(12)
+TDS:Upgrade(12)
+TDS:Upgrade(8)
+TDS:Upgrade(8)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+TDS:Upgrade(6)
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:Upgrade(11)
+TDS:Upgrade(11)
+TDS:Upgrade(12)
+TDS:Upgrade(5)
+
+-- [ Wave 15 ] --
+TDS:Upgrade(6)
+TDS:Upgrade(8)
+TDS:Upgrade(9)
+TDS:Upgrade(10)
+
+-- [ Wave 16 ] --
+TDS:Upgrade(11)
+TDS:Place("Crook Boss", 7.630661964416504, 21.3303124904632568, 22.637432098388672)
+TDS:Upgrade(13)
+
+-- [ Wave 17 ] --
+TDS:Upgrade(13)
+TDS:Place("Boomerang", 10.651361465454102, 20.8180601596832275, 19.131851196289062)
+TDS:Upgrade(14)
+TDS:Upgrade(14)
+TDS:Upgrade(14)
+
+-- [ Wave 18 ] --
+TDS:Place("Crook Boss", 4.573237419128418, 21.3544611930847168, 22.418724060058594)
+TDS:Upgrade(15)
+TDS:Upgrade(15)
+
+-- [ Wave 19 ] --
+TDS:Place("Crook Boss", 3.459913730621338, 21.417536735534668, 27.836002349853516)
+TDS:Upgrade(16)
+TDS:Upgrade(16)
+TDS:Place("Crook Boss", 6.4095869064331055,21.0205261707305908, 30.87203025817871)
+
+-- [ Wave 20 ] --
+TDS:Upgrade(17)
+TDS:Upgrade(17)
+
+-- [ Wave 21 ] --
+TDS:Place("Boomerang", 7.404476165771484, 21.0205261707305908, 19.219879150390625)
+TDS:Upgrade(18)
+TDS:Upgrade(18)
+TDS:Upgrade(18)
+TDS:Place("Boomerang", 4.306492328643799, 21.0205261707305908, 18.941843032836914)
+TDS:Upgrade(19)
+TDS:Upgrade(19)
+TDS:Upgrade(19)
+TDS:Place("Boomerang", 13.519489288330078, 21.0205261707305908, 20.37607765197754)
+TDS:Upgrade(20)
+TDS:Upgrade(20)
